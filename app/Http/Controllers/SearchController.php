06c5a968399bfa6230feb1670ceaec003f2decd6
qudi <shintarou0327@yahoo.co.jp>
2021-05-31 16:41:46 +0900

    searchコントローラの修正

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Evaluation;
 use App\Models\Genre;
 use App\Models\Location;
 use App\Models\Restaurant;
@@ -23,6 +24,24 @@ class SearchController extends Controller
             $query->where('genre_id',Genre::where('name',$request->genre)->first()->id)->get();
         }
         $items = $query->get();
+        foreach ($items as $item) {
+            $item->location =
+                Location::where('id', $item->location_id)->first()->name;
+            $item->genre =
+                Genre::where('id', $item->genre_id)->first()->name;
+            $evaluations = Evaluation::where('restaurant_id', $item->id)->get();
+            if (!$evaluations->isEmpty()) {
+                $sum = 0;
+                $count = 0;
+                foreach ($evaluations as $evaluation) {
+                    $sum += $evaluation->rating;
+                    $count += 1;
+                    $item->rating = $sum / $count;
+                }
+            } else {
+                $item->rating = 0;
+            }
+        }
         if(!$items->isEmpty()){
             return response()->json([
                 'data' => $items
