da2564862b81ae282bbcde179c9002db59e9441f
qudi <shintarou0327@yahoo.co.jp>
2021-06-15 14:49:30 +0900

    evaluationsortから受け取るデータでid

--- database/Sql/evaluation/userSort.sql
+++ database/Sql/evaluation/userSort.sql
@@ -1,1 +1,8 @@
-select evaluations.id,evaluations.restaurant_id,evaluations.user_id,evaluations.rating,evaluations.content,restaurants.name as restaurant_name,users.name as user_name from evaluations inner join restaurants on evaluations.restaurant_id = restaurants.id inner join users on evaluations.user_id = users.id where users.id =  
+select 
+evaluations.id,evaluations.restaurant_id,evaluations.user_id,evaluations.rating,evaluations.content,restaurants.name as restaurant_name,users.name as user_name 
+from 
+evaluations 
+inner join restaurants on evaluations.restaurant_id = restaurants.id 
+inner join users on evaluations.user_id = users.id 
+where 
+users.id =  
--- database/Sql/restaurant/index.sql
+++ database/Sql/restaurant/index.sql
@@ -1,1 +1,6 @@
-select restaurants.id,restaurants.name,restaurants.detail,restaurants.img,restaurants.lat,restaurants.ing,locations.name as location,genres.name as genre from restaurants inner join locations on restaurants.location_id = locations.id inner join genres on restaurants.genre_id = genres.id
+select 
+restaurants.id,restaurants.name,restaurants.detail,restaurants.img,restaurants.lat,restaurants.ing,locations.name as location,genres.name as genre 
+from 
+restaurants
+inner join locations on restaurants.location_id = locations.id
+inner join genres on restaurants.genre_id = genres.id
